8bff8fb57f84d7d0583d6450608a9a5d2590011f
jardimdanificado <jardeltorreslima01@gmail.com>
2025-02-12 22:45:49 -0300

    some changes

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+rm -rf build/data
+cp -r data build/data
 gcc -o build/brutopolis2 src/main.c -Llib -Iinclude -lbruter -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
--- src/main.c
+++ src/main.c
@@ -9,9 +9,37 @@
 enum 
 {
     ITEM_HAND,
-    ITEM_GLOCK,
-    ITEM_BULLET_9MM
+    ITEM_REVOLVER,
+    ITEM_BULLET_REVOLVER
 } items;
+
+const char* item_names[] = 
+{
+    "hand",
+    "gun_revolver",
+    "buller_revolver"
+};
+
+const char* equip_image_paths[] = 
+{
+    "data/img/equip_hand.png",
+    "data/img/equip_revolver.png",
+    "data/img/equip_bullet_revolver.png"
+};
+
+const char* item_image_paths[] = 
+{
+    "data/img/item_hand.png",
+    "data/img/item_revolver.png",
+    "data/img/item_bullet_revolver.png"
+};
+
+const int item_capacities[] = 
+{
+    0,
+    6,
+    12
+};
 
 typedef struct 
 {
@@ -28,6 +56,7 @@ typedef struct
     char* name;
     Vector3 position;
     Vector3 size;
+    Vector3 rotation;
     Vector3 direction;
     Color color;
     Float speed;
@@ -48,6 +77,7 @@ typedef struct
 {
     CreatureList *creatures;
     BulletList *bullets;
+    ItemList *items;
 } World;
 
 typedef struct 
@@ -57,6 +87,8 @@ typedef struct
     float sensibility;
 } Mouse;
 
+typedef Stack(Texture2D) TextureList;
+
 typedef struct 
 {
     char* name;
@@ -65,6 +97,8 @@ typedef struct
     World world;
     Camera camera;
     Mouse mouse;
+    TextureList *equip_textures;
+    TextureList *item_textures;
 } InternalSystem;
 
 InternalSystem* new_system(char* name, int size_x, int size_y)
@@ -82,6 +116,24 @@ InternalSystem* new_system(char* name, int size_x, int size_y)
     _sys->world.bullets = (BulletList*)malloc(sizeof(BulletList));
     stack_init(*_sys->world.bullets);
 
+    _sys->equip_textures = (TextureList*)malloc(sizeof(TextureList));
+    stack_init(*_sys->equip_textures);
+
+    _sys->item_textures = (TextureList*)malloc(sizeof(TextureList));
+    stack_init(*_sys->item_textures);
+
+    // camera setup
+    _sys->camera.position = (Vector3){ 0.0f, 1.72f, 3.0f };
+    _sys->camera.target = (Vector3){ 0.0f, 1.72f, 0.0f };
+    _sys->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    _sys->camera.fovy = 90.0f;
+    _sys->camera.projection = CAMERA_PERSPECTIVE;
+
+    // mouse setup
+    _sys->mouse.delta = (Vector2) {0,0};
+    _sys->mouse.position = (Vector2) {0,0};
+    _sys->mouse.sensibility = 0.003f;
+
     return _sys;
 }
 
@@ -98,24 +150,12 @@ void system_startup(InternalSystem* _sys)
     InitWindow(_sys->resolution.x, _sys->resolution.y, _sys->name);
     SetTargetFPS(60);
     DisableCursor();
-
-    // camera setup
-    _sys->camera.position = (Vector3){ 0.0f, 1.72f, 3.0f };
-    _sys->camera.target = (Vector3){ 0.0f, 1.72f, 0.0f };
-    _sys->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    _sys->camera.fovy = 90.0f;
-    _sys->camera.projection = CAMERA_PERSPECTIVE;
-
-    // mouse setup
-    _sys->mouse.delta = (Vector2) {0,0};
-    _sys->mouse.position = (Vector2) {0,0};
-    _sys->mouse.sensibility = 0.003f;
 }
 
 Creature* new_creature(InternalSystem* _sys, char* name, int x, int y, int z)
 {
     Creature* creature = (Creature*)malloc(sizeof(Creature));
-    creature->position = (Vector3){ GetRandomValue(-10, 10), 0.5f, GetRandomValue(-10, -5) };
+    creature->position = (Vector3){ x, y, z };
     creature->size = (Vector3){ 1.0f, 1.70f, 1.0f };
     creature->current_item = 0;
     creature->color = RED;
@@ -125,6 +165,7 @@ Creature* new_creature(InternalSystem* _sys, char* name, int x, int y, int z)
 
     creature->name = str_duplicate(name);
     creature->direction = (Vector3){0,0,0};
+    creature->rotation = (Vector3){0,0,0};
 
     stack_init(*creature->inventory);
     
@@ -142,21 +183,134 @@ Bullet* new_bullet(InternalSystem* _sys, Vector3 position, Vector3 direction, Fl
     return bullet;
 }
 
+void load_texture(InternalSystem* _sys, char* path, bool is_equip)
+{
+    Image handimg = LoadImage(path);
+    ImageResize(&handimg, 450, 300);
+    //ImageRotate(&handimg, 25.0f);
+    Texture2D texture = LoadTextureFromImage(handimg);      // Image converted to texture, uploaded to GPU memory (VRAM)
+    UnloadImage(handimg);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+
+    if (is_equip)
+        stack_push(*_sys->equip_textures, texture);
+    else
+        stack_push(*_sys->item_textures, texture);
+}
+
+Item* new_item(char* name, char type, int capacity, int content_type, int content)
+{
+    Item* item = (Item*)malloc(sizeof(Item));
+    item->type = type;
+    item->capacity = capacity;
+    item->content_type = content_type;
+    item->content = content;
+    return item;
+}
+
+void reload_item(InternalSystem* sys, Creature* creature)
+{
+    switch (creature->inventory->data[creature->current_item].type)
+    {
+        case ITEM_REVOLVER:
+            if (creature->inventory->data[creature->current_item].content == creature->inventory->data[creature->current_item].capacity)
+                return;
+            
+            for (int i = 0; i < creature->inventory->size; i++)
+            {
+                if (creature->inventory->data[i].type == ITEM_BULLET_REVOLVER)
+                {
+                    int needed = creature->inventory->data[creature->current_item].capacity - creature->inventory->data[creature->current_item].content;
+                    if (creature->inventory->data[i].content >= needed)
+                    {
+                        creature->inventory->data[creature->current_item].content += needed;
+                        creature->inventory->data[i].content -= needed;
+                        if (creature->inventory->data[i].content == 0)
+                            stack_fast_remove(*creature->inventory, i);// remove the empty item
+                        break;
+                    }
+                    else
+                    {
+                        creature->inventory->data[creature->current_item].content += creature->inventory->data[i].content;
+                        creature->inventory->data[i].content = 0;
+                        stack_fast_remove(*creature->inventory, i);// remove the empty item
+                        i--;// we need to check the same index again
+                    }
+                }
+            }
+            break;
+        default:
+            // need a message system to show messages to the player
+            // "can't reload this item"
+            break;
+    }
+}
+
+void use_item(InternalSystem* sys, Creature* creature)
+{
+    switch (creature->inventory->data[creature->current_item].type)
+    {
+    case ITEM_HAND:
+        break;
+    case ITEM_REVOLVER:
+
+        if (creature->inventory->data[creature->current_item].content == 0)
+        {
+            // need a message system to show messages to the player
+            // "no bullets"
+            // try to reload
+            reload_item(sys,creature);
+            return;
+        }
+
+        creature->inventory->data[creature->current_item].content--;
+
+        Bullet bullet = {0};
+        bullet.position = (Vector3){creature->position.x, creature->position.y + 1.7f, creature->position.z};
+        bullet.direction = (Vector3){creature->direction.x, creature->direction.y, creature->direction.z};
+        bullet.speed = BULLET_SPEED;
+        stack_push(*sys->world.bullets, bullet);
+        break;
+    case ITEM_BULLET_REVOLVER:
+        break;
+    default:
+        break;
+    }
+}
+
 int main(void)
 {
     InternalSystem* sys = new_system("brutopolis 3", 800, 600);
 
     system_startup(sys);
 
-    Image handimg = LoadImage("data/img/glock-hand.png"); 
-    ImageResize(&handimg, 300, 200);
-    ImageRotate(&handimg, 25.0f);
-    Texture2D handtexture = LoadTextureFromImage(handimg);      // Image converted to texture, uploaded to GPU memory (VRAM)
-    UnloadImage(handimg);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+    load_texture(sys, "data/img/item_hand.png", false);
+    load_texture(sys, "data/img/equip_hand.png", true);
+    load_texture(sys, "data/img/item_revolver.png", false);
+    load_texture(sys, "data/img/equip_revolver.png", true);
+    load_texture(sys, "data/img/item_bullet_revolver.png", false);
+    load_texture(sys, "data/img/equip_bullet_revolver.png", true);
 
-    new_creature(sys, "joao451", 0, 0, 0);
+    Texture2D creaturetexture = LoadTexture("data/img/creature.png");
+
+    Rectangle source = { 0.0f, 0.0f, (float)creaturetexture.width, (float)creaturetexture.height };
+
+    // NOTE: Billboard locked on axis-Y
+    Vector3 billUp = { 0.0f, 1.0f, 0.0f };
+
+    // Set the height of the rotating billboard to 1.0 with the aspect ratio fixed
+    Vector2 size = { source.width/source.height, 1.0f };
+
+    Creature* player = new_creature(sys, "joao451", 0, 0, 0);
+    Item* hand = new_item("hand", ITEM_HAND, 0, 0, 0);
+    Item* revolver = new_item("revolver", ITEM_REVOLVER, item_capacities[ITEM_REVOLVER], ITEM_BULLET_REVOLVER, 6);
+    Item* bullet_revolver = new_item("buller_revolver", ITEM_BULLET_REVOLVER, item_capacities[ITEM_BULLET_REVOLVER], 0, item_capacities[ITEM_BULLET_REVOLVER]);
+
+    stack_push(*player->inventory, *hand);
+    stack_push(*player->inventory, *revolver);
+    stack_push(*player->inventory, *bullet_revolver);
+
     
-    for (int i = 0;i < GetRandomValue(2,10);i++)
+    for (int i = 0;i < GetRandomValue(2,100);i++)
     {
         char* _name = TextFormat("joao%d", i);
         new_creature(sys, _name, GetRandomValue(-20,20), 0, GetRandomValue(-20,20));
@@ -164,30 +318,26 @@ int main(void)
 
     sys->player_index = 0;
 
-    // Controles da câmera
-    float cameraYaw = 0.0f;
-    float cameraPitch = 0.0f;
-
     while (!WindowShouldClose())
     {
         // Atualização da câmera
         // Rotação com mouse
         sys->mouse.delta = GetMouseDelta();
-        cameraYaw += sys->mouse.delta.x * sys->mouse.sensibility;
-        cameraPitch -= sys->mouse.delta.y * sys->mouse.sensibility;
+        player->rotation.x += sys->mouse.delta.x * sys->mouse.sensibility;
+        player->rotation.y -= sys->mouse.delta.y * sys->mouse.sensibility;
 
         // Limitar rotação vertical
-        cameraPitch = Clamp(cameraPitch, -PI/2.5f, PI/2.5f);
+        player->rotation.y = Clamp(player->rotation.y, -PI/2.5f, PI/2.5f);
 
         // Calcular direção da câmera
-        Vector3 direction = 
+        player->direction = (Vector3)
         {
-            cosf(cameraYaw) * cosf(cameraPitch),
-            sinf(cameraPitch),
-            sinf(cameraYaw) * cosf(cameraPitch)
+            cosf(player->rotation.x) * cosf(player->rotation.y),
+            sinf(player->rotation.y),
+            sinf(player->rotation.x) * cosf(player->rotation.y)
         };
-        direction = Vector3Normalize(direction);
-        sys->camera.target = Vector3Add(sys->camera.position, direction);
+        player->direction = Vector3Normalize(player->direction);
+        sys->camera.target = Vector3Add(sys->camera.position, player->direction);
 
         // Movimento do jogador
         Vector3 move = {0};
@@ -195,29 +345,36 @@ int main(void)
         if (IsKeyDown(KEY_S)) move.x = -1;
         if (IsKeyDown(KEY_A)) move.z = -1;
         if (IsKeyDown(KEY_D)) move.z = 1;
+        // mouse wheel to scroll through items
+        int mouse_wheel = GetMouseWheelMove();
+        if (mouse_wheel != 0)
+        {
+            player->current_item += mouse_wheel;
+            player->current_item = Clamp(player->current_item, 0, player->inventory->size - 1);
+        }
 
         // Rotacionar movimento de acordo com a direção da câmera
         Vector3 rotatedMove = 
         {
-            move.x * cosf(cameraYaw) - move.z * sinf(cameraYaw),
+            move.x * cosf(player->rotation.x) - move.z * sinf(player->rotation.x),
             0.0f,
-            move.x * sinf(cameraYaw) + move.z * cosf(cameraYaw)
+            move.x * sinf(player->rotation.x) + move.z * cosf(player->rotation.x)
         };
-        sys->world.creatures->data[sys->player_index].position = Vector3Add(sys->world.creatures->data[sys->player_index].position, Vector3Scale(rotatedMove, sys->world.creatures->data[sys->player_index].speed));
+        player->position = Vector3Add(player->position, Vector3Scale(rotatedMove, player->speed));
 
         sys->camera.position = Vector3Add(
-            sys->world.creatures->data[sys->player_index].position,
+            player->position,
             (Vector3){0.0f, 1.72f, 0.0f} // Offset de altura dos olhos
         );
 
         // Atirar
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) 
         {
-            Bullet bullet = {0};
-            bullet.position = (Vector3){sys->camera.position.x, sys->camera.position.y, sys->camera.position.z};
-            bullet.direction = direction;
-            bullet.speed = BULLET_SPEED;
-            stack_push(*sys->world.bullets, bullet);
+            use_item(sys,player);
+        }
+        else if (IsKeyDown(KEY_R))
+        {
+            reload_item(sys,player);
         }
 
         // Atualizar balas
@@ -229,8 +386,8 @@ int main(void)
             for (int j = 0; j < sys->world.creatures->size; j++) 
             {
                 if (CheckCollisionBoxSphere(
-                (BoundingBox){(Vector3){sys->world.creatures->data[j].position.x - 0.5f, sys->world.creatures->data[j].position.y - 0.5f, sys->world.creatures->data[j].position.z - 0.5f},
-                            (Vector3){sys->world.creatures->data[j].position.x + 0.5f, sys->world.creatures->data[j].position.y + 0.5f, sys->world.creatures->data[j].position.z + 0.5f}},
+                (BoundingBox){(Vector3){sys->world.creatures->data[j].position.x - 0.4f, sys->world.creatures->data[j].position.y, sys->world.creatures->data[j].position.z - 0.5f},
+                            (Vector3){sys->world.creatures->data[j].position.x + 0.4f, sys->world.creatures->data[j].position.y + 1.93, sys->world.creatures->data[j].position.z + 0.5f}},
                 sys->world.bullets->data[i].position,
                 0.1f))
                 {
@@ -248,7 +405,7 @@ int main(void)
             // Desativar balas muito longe
             if (Vector3Distance(sys->camera.position, sys->world.bullets->data[i].position) > 50) 
             {
-                stack_remove(*sys->world.bullets, i);
+                stack_fast_remove(*sys->world.bullets, i);
             }
         }
 
@@ -262,8 +419,9 @@ int main(void)
                 // Inimigos
                 for (int i = 0; i < sys->world.creatures->size; i++) 
                 {
+                    // size 1x1.7x1
                     if (i != sys->player_index)
-                        DrawCube(sys->world.creatures->data[i].position, 1.0f, 1.0f, 1.0f, sys->world.creatures->data[i].color);
+                        DrawBillboardPro(sys->camera, creaturetexture, source, (Vector3){sys->world.creatures->data[i].position.x, sys->world.creatures->data[i].position.y + 0.85f, sys->world.creatures->data[i].position.z}, billUp, (Vector2){1.0f, 1.7f}, (Vector2){0.5f, 0.5f}, 0, WHITE);
                 }
 
                 // Balas
@@ -278,7 +436,8 @@ int main(void)
             DrawLine(sys->resolution.x/2 - 10, sys->resolution.y/2, sys->resolution.x/2 + 10, sys->resolution.y/2, GREEN);
             DrawLine(sys->resolution.x/2, sys->resolution.y/2 - 10, sys->resolution.x/2, sys->resolution.y/2 + 10, GREEN);
 
-            DrawTexture(handtexture, sys->resolution.x - handtexture.width + 70, sys->resolution.y - handtexture.height+25, WHITE);
+            DrawTexture(sys->equip_textures->data[player->current_item],
+            sys->resolution.x - sys->equip_textures->data[player->current_item].width + 70, sys->resolution.y - sys->equip_textures->data[player->current_item].height+25, WHITE);
 
             //DrawText(TextFormat("Inimigos restantes: %d", enemies->size), 10, 10, 20, DARKGRAY);
 
